228bfc63cb847e59271c0a698323e84752a719ce
suhani86 <suhani235>
2024-10-06 18:22:14 +0530

    Initial commit

---
+++ portfolio/src/Details.js
@@ -0,0 +1,168 @@
+// Enter all your detials in this file
+// Logo images
+
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Suhani Bhatnagar",
+  tagline: "I build things for web",
+  img: profile,
+  about: `I am currenty pursuing BCA from Sant Hirdaram Girls College and I am in my final year. During my studies
+  I developed a interest in coding and building logic. my hobbies are reading books and travelling.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/",
+  github: "https://github.com/suhani86/",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Web Technologies",
+    Company: `Techdev`,
+    Location: "Bhopal",
+    Type: "Certificate",
+    Duration: "Aug 2024 - Sep 2024",
+  },
+  {
+    Position: "Advance Python",
+    Company: `Techdev`,
+    Location: "Bhopal",
+    Type: "Certificate",
+    Duration: "July 2023 - Aug 2023",
+  },
+  {
+    Position: " Basics of Python",
+    Company: `Techdev`,
+    Location: "Bhopal",
+    Type: "Certificate",
+    Duration: "dec 2022 - jan 2022",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor's of Computer Application",
+    Company: "Sant Hirdaram Girls College",
+    Location: "Bhopal",
+    Type: "Full Time",
+    Duration: "June 2022 - Present",
+  },
+  {
+    Position: "Senior Secondary",
+    Company: `Sardar Patel Higher Secondary School`,
+    Location: "Bhopal",
+    Type: "Full Time",
+    Duration: "Aug 2021 - jun 2022",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  
+  bootstrap: bootstrap,
+  vscode: vscode,
+  
+  npm: npm,
+  git: git,
+  github: github,
+  
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "suhani2305@gmail.com",
+  phone: "+91 93991 22842",
+};
